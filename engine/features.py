936021babe5b57401fe136c2ac668704b1b6ca10
judinus10 <judes.20222403@iit.ac.lk>
2024-08-26 15:26:51 +0530

    Added jarvis sound effects

---
+++ engine/features.py
@@ -0,0 +1,6 @@
+from playsound import playsound
+
+#playing assistant sound function
+def playAssistantSound():
+    music_dir= "www\\assets\\audio\\www_assets_audio_start_sound.mp3"
+    playsound(music_dir)

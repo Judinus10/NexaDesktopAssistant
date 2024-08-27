05e13dcdb23b6614f470e3b3b46fe15d5eb84f04
judinus10 <judes.20222403@iit.ac.lk>
2024-08-27 15:47:15 +0530

    added speach recognizing

--- engine/_pycache_/command.py
+++ engine/_pycache_/command.py
@@ -1,4 +1,7 @@
 import pyttsx3
+import speech_recognition as sr
+import pyaudio
+
 
 def speak(text):
     engine = pyttsx3.init()
@@ -7,5 +10,29 @@ def speak(text):
     engine.setProperty('rate',174)
     engine.say(text)
     engine.runAndWait()
+
+def takecommand():
+    
+    r = sr.Recognizer()
+
+    with sr.Microphone() as source:
+
+        print("Listning....")
+        r.pause_threshold = 1
+        r.adjust_for_ambient_noise(source)
+
+        audio = r.listen(source,10,6)
+
+    try:
+        print("recognizing")
+        query = r.recognize_google(audio,language ='en-in')
+        print(f"user said : (query)")
+    except Exception as e:
+        return " "
+    return query.lower()
+
+text =takecommand()
+
+speak(text)
 
 speak("I'm Jarvis")

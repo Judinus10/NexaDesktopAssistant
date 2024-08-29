a16aebb67ae2d609c4ddcfb93b7a9248fa762ded
judinus10 <judes.20222403@iit.ac.lk>
2024-08-29 11:39:43 +0530

    open apps using terminal

--- engine/_pycache_/command.py
+++ engine/_pycache_/command.py
@@ -11,7 +11,6 @@ def speak(text):
     engine.say(text)
     engine.runAndWait()
 
-@eel.expose
 def takecommand():
     
     r = sr.Recognizer()
@@ -36,3 +35,14 @@ def takecommand():
     except Exception as e:
         return " "
     return query.lower()
+
+@eel.expose
+def allCommands():
+
+    query = takecommand()
+    print(query)
+
+    if open in query:
+        print("I Run")
+    else :
+        print("Not Run")
--- engine/_pycache_/features.py
+++ engine/_pycache_/features.py
@@ -1,5 +1,8 @@
+import os
 from playsound import playsound
 import eel
+from pyttsx3 import speak
+from engine._pycache_.config import ASSISTANT_NAME
 
 #playing assistant sound function
 
@@ -7,3 +10,14 @@ import eel
 def playAssistantSound():
     music_dir= "www\\assets\\audio\\www_assets_audio_start_sound.mp3"
     playsound(music_dir)
+
+def openCommand(query):
+    query = query.replace(ASSISTANT_NAME, "")
+    query = query.replace("open","")
+    query.lower()
+
+    if query!= "":
+        speak("Opening"+query)
+        os.speak("start"+query)
+    else:
+        speak("not found")

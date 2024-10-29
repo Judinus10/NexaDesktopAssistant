0a7c100909217f1966bafd0479685d671c68dc44
judinus10 <judes.20222403@iit.ac.lk>
2024-10-29 09:44:56 +0530

    try to install pvporcupine but ended in fail . hot word fail

--- engine/_pycache_/features.py
+++ engine/_pycache_/features.py
@@ -8,7 +8,7 @@ from engine._pycache_.config import ASSISTANT_NAME
 import pywhatkit as kit
 import webbrowser
 from engine._pycache_.db import cursor
-import pvporcupine 
+# import pvporcupine 
 import pyaudio
 import struct
 import time
@@ -86,40 +86,40 @@ def PlayYoutube(query):
     os.system(f"start {url}")
 
 
-def hotword():
-    porcupine=None
-    paud=None
-    audio_stream=None
-    try:
+# def hotword():
+#     porcupine=None
+#     paud=None
+#     audio_stream=None
+#     try:
        
-        # pre trained keywords    
-        porcupine=pvporcupine.create(keywords=["jarvis","alexa"]) 
-        paud=pyaudio.PyAudio()
-        audio_stream=paud.open(rate=porcupine.sample_rate,channels=1,format=pyaudio.paInt16,input=True,frames_per_buffer=porcupine.frame_length)
+#         # pre trained keywords    
+#         porcupine=pvporcupine.create(keywords=["jarvis","alexa"]) 
+#         paud=pyaudio.PyAudio()
+#         audio_stream=paud.open(rate=porcupine.sample_rate,channels=1,format=pyaudio.paInt16,input=True,frames_per_buffer=porcupine.frame_length)
         
-        # loop for streaming
-        while True:
-            keyword=audio_stream.read(porcupine.frame_length)
-            keyword=struct.unpack_from("h"*porcupine.frame_length,keyword)
+#         # loop for streaming
+#         while True:
+#             keyword=audio_stream.read(porcupine.frame_length)
+#             keyword=struct.unpack_from("h"*porcupine.frame_length,keyword)
 
-            # processing keyword comes from mic 
-            keyword_index=porcupine.process(keyword)
+#             # processing keyword comes from mic 
+#             keyword_index=porcupine.process(keyword)
 
-            # checking first keyword detetcted for not
-            if keyword_index>=0:
-                print("hotword detected")
+#             # checking first keyword detetcted for not
+#             if keyword_index>=0:
+#                 print("hotword detected")
 
-                # pressing shorcut key win+j
-                import pyautogui as autogui
-                autogui.keyDown("win")
-                autogui.press("b")
-                time.sleep(2)
-                autogui.keyUp("win")
+#                 # pressing shorcut key win+j
+#                 import pyautogui as autogui
+#                 autogui.keyDown("win")
+#                 autogui.press("b")
+#                 time.sleep(2)
+#                 autogui.keyUp("win")
                 
-    except:
-        if porcupine is not None:
-            porcupine.delete()
-        if audio_stream is not None:
-            audio_stream.close()
-        if paud is not None:
-            paud.terminate()
+#     except:
+#         if porcupine is not None:
+#             porcupine.delete()
+#         if audio_stream is not None:
+#             audio_stream.close()
+#         if paud is not None:
+#             paud.terminate()

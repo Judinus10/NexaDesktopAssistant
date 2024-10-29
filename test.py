57a18897fd004acdc5b6eb5f66d0dd670abcd69c
judinus10 <judes.20222403@iit.ac.lk>
2024-10-29 11:41:49 +0530

    comment the hotword part

--- test.py
+++ test.py
@@ -1,44 +1,8 @@
-import time
-import pyaudio
-import pvporcupine
-import struct
+import pocketsphinx 
+from pocketsphinx import LiveSpeech
 
-def hotword(): 
-    porcupine=None
-    paud=None
-    audio_stream=None
-    try:
-        
-            # pre trained keywords    
-            porcupine=pvporcupine.create(keywords=["jarvis","alexa"]) 
-            paud=pyaudio.PyAudio()
-            audio_stream=paud.open(rate=porcupine.sample_rate,channels=1,format=pyaudio.paInt16,input=True,frames_per_buffer=porcupine.frame_length)
-            
-            # loop for streaming
-            while True:
-                keyword=audio_stream.read(porcupine.frame_length)
-                keyword=struct.unpack_from("h"*porcupine.frame_length,keyword)
+# Create a LiveSpeech instance with the desired language model
+speech = LiveSpeech(lm=False, keyphrase='Nexa', kws_threshold=1e-20)
 
-                # processing keyword comes from mic 
-                keyword_index=porcupine.process(keyword)
-
-                # checking first keyword detetcted for not
-                if keyword_index>=0:
-                    print("hotword detected")
-
-                    # pressing shorcut key win+j
-                    import pyautogui as autogui
-                    autogui.keyDown("win")
-                    autogui.press("b")
-                    time.sleep(2)
-                    autogui.keyUp("win")
-
-    except:
-            if porcupine is not None:
-                porcupine.delete()
-            if audio_stream is not None:
-                audio_stream.close()
-            if paud is not None:
-                paud.terminate()
-
-hotword()
+for phrase in speech:
+    print("Hotword detected: ", phrase)

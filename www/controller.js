6dd96b5ea7187d928dcb745e97c3e28189a25473
judinus10 <judes.20222403@iit.ac.lk>
2024-11-02 20:49:50 +0530

    File added

--- www/controller.js
+++ www/controller.js
@@ -32,18 +32,31 @@ $(document).ready(function () {
 
     eel.expose(receiverText)
     function receiverText(message) {
-
         var chatBox = document.getElementById("chat-canvas-body");
         if (message.trim() !== "") {
             chatBox.innerHTML += `<div class="row justify-content-start mb-4">
             <div class = "width-size">
             <div class="receiver_message">${message}</div>
-            </div>
         </div>`; 
     
             // Scroll to the bottom of the chat box
             chatBox.scrollTop = chatBox.scrollHeight;
         }
-        console.log("Receiver text added:", message)
     }
+
+    // eel.expose(receiverText)
+    // function receiverText(message) {
+
+    //     var chatBox = document.getElementById("chat-canvas-body");
+    //     if (message.trim() !== "") {
+    //         chatBox.innerHTML += `<div class="row justify-content-start mb-4">
+    //         <div class = "width-size">
+    //         <div class="receiver_message">${message}</div>
+    //         </div>
+    //     </div>`; 
+    
+    //         // Scroll to the bottom of the chat box
+    //         chatBox.scrollTop = chatBox.scrollHeight;
+    //     }
+    // }
 });
